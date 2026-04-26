3fcec4b7d7627a759852545c15d37ad0b2267fe4
tlaf8 <21laforgth@gmail.com>
2026-04-26 17:59:44 -0600

    refactor: clean up blink example
    1) Deleted unused files
    2) Change implementation to use new led component

--- blink/main/blink.cpp
+++ blink/main/blink.cpp
@@ -1,25 +1,25 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "led_strip.h"
-#include "portmacro.h"
 #include <string.h>
-#include "ws2812.hpp"
+#include "ws2812/ws2812.hpp"
+
+static ws2812 led{21};
 
 static void blink_task(void *arg) {
-    WS2812 onboard_led(WS2812_ONBOARD_LED_PIN);
-
     for(;;) {
-        onboard_led.set({255, 0, 0});
+        led.set(255, 0, 0);
         vTaskDelay(pdMS_TO_TICKS(250));
-        onboard_led.set({0, 255, 0});
+        led.set(0, 255, 0);
         vTaskDelay(pdMS_TO_TICKS(250));
-        onboard_led.set({0, 0, 255});
+        led.set(0, 0, 255);
         vTaskDelay(pdMS_TO_TICKS(250));
-        onboard_led.set({255, 255, 255});
+        led.set(255, 255, 255);
         vTaskDelay(pdMS_TO_TICKS(250));
     }
 }
 
 extern "C" void app_main(void) {
+    led.init();
+
     xTaskCreate(blink_task, "blink", 2048, NULL, 5, NULL);
 }
